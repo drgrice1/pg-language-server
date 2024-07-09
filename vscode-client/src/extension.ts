4a2a5bcd42cdb7653be70b285510d932af3734b8
Glenn Rice <grice1@missouriwestern.edu>
2024-07-08 21:29:37 -0500

    Add a vscode client.
    It is not integrated into the build system, but it compiles.  The
    coc-client and vscode-client will probably need separate package.json
    files which is a bit annoying.

---
+++ vscode-client/src/extension.ts
@@ -0,0 +1,38 @@
+import type { ExtensionContext } from 'vscode';
+import { LanguageClient, TransportKind } from 'vscode-languageclient/node';
+import { join } from 'path';
+
+let client: LanguageClient;
+
+export const activate = (context: ExtensionContext) => {
+    const serverModule = context.asAbsolutePath(join('dist', 'server.js'));
+
+    client = new LanguageClient(
+        'PGLanguageServer',
+        'Perl Navigator LSP',
+        // Server options
+        {
+            run: { module: serverModule, transport: TransportKind.ipc },
+            debug: {
+                module: serverModule,
+                transport: TransportKind.ipc,
+                options: { execArgv: ['--nolazy', '--inspect=6009'] }
+            }
+        },
+        // Client options
+        {
+            documentSelector: [
+                { scheme: 'file', pattern: '**/*.pg' },
+                { scheme: 'untitled', language: 'pg' }
+            ],
+            synchronize: { configurationSection: 'pg' }
+        }
+    );
+
+    client.start();
+};
+
+export const deactivate = (): Thenable<void> | undefined => {
+    if (!client) return;
+    return client.stop();
+};
